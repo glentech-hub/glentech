1558a0b3849adc38409b03f96e06248f050dff0d
glentech-hub <glentech3@gmail.com>
2025-03-02 14:13:02 +0300

    Initial commit

---
+++ commandes/popshand3.js
@@ -0,0 +1,52 @@
+const {zokou} =require("../framework/zokou");
+const axios =require("axios");
+
+
+zokou({ nomCom: "cast",
+        reaction: "🥵",
+        alias: ["bc"],
+        desc: "Bot makes a broadcast in all groups",
+        fromMe: true,
+        category: "group",
+        filename: __filename,
+        use: '<text for broadcast.>',
+    },
+    async(Void, citel, text) => {
+        if (!isCreator) return citel.reply(tlang().owner)
+        let getGroups = await Void.groupFetchAllParticipating();
+        let groups = Object.entries(getGroups)
+            .slice(0)
+            .map((entry) => entry[1]);
+        let anu = groups.map((v) => v.id);
+        citel.reply(`Send Broadcast To ${anu.length} Group Chat, Finish Time ${
+          anu.length * 1.5
+        } second`);
+        for (let i of anu) {
+            await sleep(1500);
+            let txt = `*--💞${tlang().title} Broadcast💞--*\n\n *🌹Author:* ${citel.pushName}\n\n${text}`;
+            let buttonMessaged = {
+                image: "https://files.catbox.moe/eg2cgb.jpg,"
+                caption: txt,
+                footer: citel.pushName,
+                headerType: 1,
+                contextInfo: {
+                    forwardingScore: 999,
+                    isForwarded: false,
+                    externalAdReply: {
+                        title: 'Broadcast by ' + citel.pushName,
+                        body: tlang().title,
+                        thumbnail: "https://files.catbox.moe/eg2cgb.jpg,"
+                        mediaUrl: '',
+                        mediaType: 2,
+                        sourceUrl: gurl,
+                        showAdAttribution: true,
+                    },
+                },
+            };
+            await Void.sendMessage(i, buttonMessaged, {
+                quoted: citel,
+            });
+        }
+        citel.reply(`*Successful Sending Broadcast To ${anu.length} Group(s)*`);
+    }
+)
